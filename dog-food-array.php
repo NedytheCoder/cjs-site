cf4fa1165b3bc26aecdf853b43686650846595ed
unknown <NedytheCoder>
2022-04-28 20:51:36 +0100

    initial commit

---
+++ dog-food-array.php
@@ -0,0 +1,66 @@
+<?php
+    $pup_food = [
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED PUPPY",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED PUPPY",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED PUPPY",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED PUPPY",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        ];
+
+    $adu_food = [
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED ADULT",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED ADULT",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED ADULT",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+        [
+            "image" => "./img/446d3a45405039.582f47d703c5b-removebg-preview.png",
+            "lead-title" => "Healthy Gourmet",
+            "title" => "LARGE BREED ADULT",
+            "description" => "lorem ipsum dolor sit amet, consectetur adipis, sed diam nonumy eirmod tempor.",
+            "price" => number_format(1250000),
+        ],
+    ]
+
+
+
+?>
